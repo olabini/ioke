60c70ab4e3368a8abed8dfa4ea003a121717aaf4
Ola Bini <ola.bini@gmail.com>
2008-09-30 11:25:19 +0200

    Add basic shell script and empty main class + license information

---
+++ src/main/ioke/lang/Main.java
@@ -0,0 +1,17 @@
+/*
+ * See LICENSE file in distribution for copyright and licensing information.
+ */
+package ioke.lang;
+
+/**
+ * @author <a href="mailto:ola.bini@gmail.com">Ola Bini</a>
+ */
+public class Main {
+    public Main() {
+        //TODO: Add implementation
+    }
+
+    public static void main(String[] args) throws Exception {
+        
+    }
+}// Main
