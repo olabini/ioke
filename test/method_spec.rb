eb8d20533812d090adca80a6213e4bad967ae669
Ola Bini <ola.bini@gmail.com>
2008-10-04 15:29:53 +0200

    Adding specs for some method features

--- test/method_spec.rb
+++ test/method_spec.rb
@@ -5,9 +5,17 @@ import Java::java.io.StringReader unless defined?(StringReader)
 describe "DefaultBehavior method" do 
   it "should return a method that returns nil when called with no arguments" do 
     ioke = IokeRuntime.get_runtime()
+    ioke.evaluate_stream(StringReader.new("method call")).should == ioke.nil
     ioke.evaluate_stream(StringReader.new("method() call")).should == ioke.nil
   end
   
-  it "should name itself after the slot it's assigned to if it has no name"
-  it "should not change it's name if it already has a name"
+  it "should name itself after the slot it's assigned to if it has no name" do 
+    ioke = IokeRuntime.get_runtime()
+    ioke.evaluate_stream(StringReader.new("x = method(nil)")).name.should == "x"
+  end
+  
+  it "should not change it's name if it already has a name" do 
+    ioke = IokeRuntime.get_runtime()
+    ioke.evaluate_stream(StringReader.new("x = method(nil)\ny = getCell(\"x\")\ngetCell(\"y\")")).name.should == "x"
+  end
 end
