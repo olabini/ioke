ab15780fe9b9f85ac4d305f42b7a252422505a5d
Ola Bini <ola.bini@gmail.com>
2008-10-14 14:14:17 +0200

    Add support for explicit terminators

--- test/parsing_spec.rb
+++ test/parsing_spec.rb
@@ -8,6 +8,43 @@ def parse(str)
 end
 
 describe "parsing" do 
+  describe "terminators" do 
+    it "should parse a newline as a terminator" do 
+      m = parse("\n").to_string
+      m.should == ";\n"
+    end
+
+    it "should parse two newlines as one terminator" do 
+      m = parse("\n\n").to_string
+      m.should == ";\n"
+    end
+
+    it "should parse a semicolon as a terminator" do 
+      m = parse(";").to_string
+      m.should == ";\n"
+    end
+
+    it "should parse two semicolons as one terminator" do 
+      m = parse(";;").to_string
+      m.should == ";\n"
+    end
+
+    it "should parse one semicolon and one newline as one terminator" do 
+      m = parse(";\n").to_string
+      m.should == ";\n"
+    end
+
+    it "should parse one newline and one semicolon as one terminator" do 
+      m = parse("\n;").to_string
+      m.should == ";\n"
+    end
+
+    it "should parse one newline and one semicolon and one newline as one terminator" do 
+      m = parse("\n;\n").to_string
+      m.should == ";\n"
+    end
+  end
+
   describe "parens without preceeding message" do 
     it "should be translated into identity message" do 
       m = parse("(1)").to_string
