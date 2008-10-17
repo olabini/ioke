cb4a8c89d2c5760d0832a90f39af8d655e32f517
Ola Bini <ola.bini@gmail.com>
2008-10-17 20:27:08 +0200

    Make ground a data-object, part of refactoring

--- src/main/ioke/lang/Ground.java
+++ src/main/ioke/lang/Ground.java
@@ -9,35 +9,24 @@ package ioke.lang;
  *
  * @author <a href="mailto:ola.bini@gmail.com">Ola Bini</a>
  */
-public class Ground extends IokeObject {
-    Ground(Runtime runtime, String documentation) {
-        super(runtime, documentation);
-    }
-
-    @Override
-    public IokeObject allocateCopy(Message m, IokeObject context) {
-        return new Ground(runtime, documentation);
-    }
-
-    public void init() {
-        registerCell("Base", runtime.base);
-        registerCell("DefaultBehavior", runtime.defaultBehavior);
-        registerCell("Ground", runtime.ground);
-        registerCell("Origin", runtime.origin);
-        registerCell("System", runtime.system);
-        registerCell("Runtime", runtime.runtime);
-        registerCell("Text", runtime.text);
-        registerCell("Number", runtime.number);
-        registerCell("nil", runtime.nil);
-        registerCell("true", runtime._true);
-        registerCell("false", runtime._false);
-        registerCell("Method", runtime.method);
-        registerCell("DefaultMethod", runtime.defaultMethod);
-        registerCell("JavaMethod", runtime.javaMethod);
-        registerCell("Mixins", runtime.mixins);
-    }
-
-    public String toString() {
-        return "Ground";
+public class Ground {
+    public static void init(IokeObject ground) {
+        Runtime runtime = ground.runtime;
+        ground.setKind("Ground");
+        ground.registerCell("Base", runtime.base);
+        ground.registerCell("DefaultBehavior", runtime.defaultBehavior);
+        ground.registerCell("Ground", runtime.ground);
+        ground.registerCell("Origin", runtime.origin);
+        ground.registerCell("System", runtime.system);
+        ground.registerCell("Runtime", runtime.runtime);
+        ground.registerCell("Text", runtime.text);
+        ground.registerCell("Number", runtime.number);
+        ground.registerCell("nil", runtime.nil);
+        ground.registerCell("true", runtime._true);
+        ground.registerCell("false", runtime._false);
+        ground.registerCell("Method", runtime.method);
+        ground.registerCell("DefaultMethod", runtime.defaultMethod);
+        ground.registerCell("JavaMethod", runtime.javaMethod);
+        ground.registerCell("Mixins", runtime.mixins);
     }
 }// Ground
