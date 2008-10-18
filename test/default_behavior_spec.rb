33e35eb8f95170854b7d3e14c53b098716e3e22c
Ola Bini <ola.bini@gmail.com>
2008-10-18 15:16:45 +0200

    Add some more pending specs

---
+++ test/default_behavior_spec.rb
@@ -0,0 +1,48 @@
+include_class('ioke.lang.Runtime') { 'IokeRuntime' } unless defined?(IokeRuntime)
+
+import Java::java.io.StringReader unless defined?(StringReader)
+
+describe "DefaultBehavior" do 
+  describe "'derive'" do 
+    it "should work exactly like mimic"
+  end
+  
+  describe "'break'" do 
+    it "should raise a control flow exception by default"
+  end
+  
+  describe "'until'" do 
+    it "should not do anything if initial argument is true"
+    it "should loop until the argument becomes true"
+    it "should be interrupted by break"
+  end
+
+  describe "'while'" do 
+    it "should not do anything if initial argument is false"
+    it "should loop until the argument becomes false"
+    it "should be interrupted by break"
+  end
+  
+  describe "'loop'" do 
+    it "should loop until interrupted by break"
+  end
+
+  describe "'if'" do 
+    it "should evaluate it's first element once"
+    it "should return it's second argument if the first element evaluates to true"
+    it "should return it's third argument if the first element evaluates to false"
+    it "should return the result of evaluating the first argument if there are no more arguments"
+    it "should return the result of evaluating the first argument if it is false and there are only two arguments"
+  end
+
+  describe "'asText'" do 
+    it "should call toString and return the text from that"
+  end
+
+  describe "'representation'" do 
+    it "should call representation and return the text from that"
+  end
+
+  describe "'method'" do 
+  end
+end
