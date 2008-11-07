bf7de299170b047c2594f83791fe92202885f2a1
Ola Bini <ola.bini@gmail.com>
2008-11-07 19:44:27 +0100

    Add spec file for Mixins, and add some pending tests for cells

---
+++ test/mixins_spec.rb
@@ -0,0 +1,15 @@
+include_class('ioke.lang.Runtime') { 'IokeRuntime' } unless defined?(IokeRuntime)
+
+describe "Mixins" do 
+  describe "'cell'" do 
+    it "should have tests"
+  end
+
+  describe "'cell='" do 
+    it "should have tests"
+  end
+
+  describe "'cells'" do 
+    it "should have tests"
+  end
+end
