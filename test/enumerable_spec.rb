afc0baa311dda61678d14450d1447f9c5db937e4
Ola Bini <ola.bini@gmail.com>
2008-12-04 14:13:43 +0100

    Add Enumerable#collect, Enumerable#mapFn and Enumerable#collectFn

--- test/enumerable_spec.rb
+++ test/enumerable_spec.rb
@@ -1,5 +1,23 @@
 include_class('ioke.lang.Runtime') { 'IokeRuntime' } unless defined?(IokeRuntime)
 
+$CUSTOM_ENUMERABLE_STRING = <<CODE
+CustomEnumerable = Origin mimic
+CustomEnumerable mimic!(Mixins Enumerable)
+CustomEnumerable each = macro(
+  len = call arguments length
+  
+  if(len == 1,
+    first = call arguments first
+    first evaluateOn(call ground, "3first")
+    first evaluateOn(call ground, "1second")
+    first evaluateOn(call ground, "2third"),
+    
+    lexical = LexicalBlock createFrom(call arguments, call ground)
+    lexical call("3first")
+    lexical call("1second")
+    lexical call("2third")))
+CODE
+
 describe "Mixins" do 
   describe "Enumerable" do 
     describe "'sort'" do 
@@ -20,22 +38,7 @@ CODE
       it "should return a list based on all things yielded to each" do 
         ioke = IokeRuntime.get_runtime
         ioke.evaluate_string(<<CODE).should == ioke.true
-CustomEnumerable = Origin mimic
-CustomEnumerable mimic!(Mixins Enumerable)
-CustomEnumerable each = macro(
-  len = call arguments length
-  
-  if(len == 1,
-    first = call arguments first
-    first evaluateOn(call ground, "3first")
-    first evaluateOn(call ground, "1second")
-    first evaluateOn(call ground, "2third"),
-    
-    lexical = LexicalBlock createFrom(call arguments, call ground)
-    lexical call("3first")
-    lexical call("1second")
-    lexical call("2third")))
-
+#$CUSTOM_ENUMERABLE_STRING
 CustomEnumerable asList == ["3first", "1second", "2third"]
 CODE
       end
@@ -64,6 +67,96 @@ CODE
         ioke = IokeRuntime.get_runtime
         ioke.evaluate_string("[1, 2, 3] map(x, x+3) == [4, 5, 6]").should == ioke.true
         ioke.evaluate_string("[1, 2, 3] map(x, 1) == [1, 1, 1]").should == ioke.true
+      end
+    end
+
+    describe "'mapFn'" do 
+      it "should take zero arguments and just return the elements in a list" do 
+        ioke = IokeRuntime.get_runtime
+        ioke.evaluate_string("[1, 2, 3] mapFn == [1, 2, 3]").should == ioke.true
+        ioke.evaluate_string(<<CODE).should == ioke.true
+#$CUSTOM_ENUMERABLE_STRING
+CustomEnumerable mapFn == ["3first", "1second", "2third"]
+CODE
+      end
+      
+      it "should take one lexical block argument and apply that to each element, and return the result in a list" do 
+        ioke = IokeRuntime.get_runtime
+        ioke.evaluate_string("x = fn(arg, arg+2). [1, 2, 3] mapFn(x) == [3, 4, 5]").should == ioke.true
+        ioke.evaluate_string(<<CODE).should == ioke.true
+#$CUSTOM_ENUMERABLE_STRING
+x = fn(arg, arg[0..2])
+CustomEnumerable mapFn(x) == ["3fi", "1se", "2th"]
+CODE
+      end
+
+      it "should take several lexical blocks and chain them together" do 
+        ioke = IokeRuntime.get_runtime
+        ioke.evaluate_string("x = fn(arg, arg+2). x2 = fn(arg, arg*2). [1, 2, 3] mapFn(x, x2) == [6, 8, 10]").should == ioke.true
+        ioke.evaluate_string(<<CODE).should == ioke.true
+#$CUSTOM_ENUMERABLE_STRING
+x = fn(arg, arg[0..2])
+x2 = fn(arg, arg + "flurg")
+CustomEnumerable mapFn(x, x2) == ["3fiflurg", "1seflurg", "2thflurg"]
+CODE
+      end
+    end
+    
+    describe "'collect'" do 
+      it "should return an empty list for an empty enumerable" do 
+        ioke = IokeRuntime.get_runtime
+        ioke.evaluate_string("[] collect(x, x+2) == []").should == ioke.true
+        ioke.evaluate_string("{} collect(x, x+2) == []").should == ioke.true
+        ioke.evaluate_string("set collect(x, x+2) == []").should == ioke.true
+      end
+      
+      it "should return the same list for something that only returns itself" do 
+        ioke = IokeRuntime.get_runtime
+        ioke.evaluate_string("[1, 2, 3] collect(x, x) == [1, 2, 3]").should == ioke.true
+      end
+
+      it "should take one argument and apply the inside" do 
+        ioke = IokeRuntime.get_runtime
+        ioke.evaluate_string("[1, 2, 3] collect(+2) == [3, 4, 5]").should == ioke.true
+        ioke.evaluate_string("[1, 2, 3] collect(. 1) == [1, 1, 1]").should == ioke.true
+      end
+
+      it "should take two arguments and apply the code with the argument name bound" do 
+        ioke = IokeRuntime.get_runtime
+        ioke.evaluate_string("[1, 2, 3] collect(x, x+3) == [4, 5, 6]").should == ioke.true
+        ioke.evaluate_string("[1, 2, 3] collect(x, 1) == [1, 1, 1]").should == ioke.true
+      end
+    end
+
+    describe "'collectFn'" do 
+      it "should take zero arguments and just return the elements in a list" do 
+        ioke = IokeRuntime.get_runtime
+        ioke.evaluate_string("[1, 2, 3] collectFn == [1, 2, 3]").should == ioke.true
+        ioke.evaluate_string(<<CODE).should == ioke.true
+#$CUSTOM_ENUMERABLE_STRING
+CustomEnumerable collectFn == ["3first", "1second", "2third"]
+CODE
+      end
+      
+      it "should take one lexical block argument and apply that to each element, and return the result in a list" do 
+        ioke = IokeRuntime.get_runtime
+        ioke.evaluate_string("x = fn(arg, arg+2). [1, 2, 3] collectFn(x) == [3, 4, 5]").should == ioke.true
+        ioke.evaluate_string(<<CODE).should == ioke.true
+#$CUSTOM_ENUMERABLE_STRING
+x = fn(arg, arg[0..2])
+CustomEnumerable collectFn(x) == ["3fi", "1se", "2th"]
+CODE
+      end
+
+      it "should take several lexical blocks and chain them together" do 
+        ioke = IokeRuntime.get_runtime
+        ioke.evaluate_string("x = fn(arg, arg+2). x2 = fn(arg, arg*2). [1, 2, 3] collectFn(x, x2) == [6, 8, 10]").should == ioke.true
+        ioke.evaluate_string(<<CODE).should == ioke.true
+#$CUSTOM_ENUMERABLE_STRING
+x = fn(arg, arg[0..2])
+x2 = fn(arg, arg + "flurg")
+CustomEnumerable collectFn(x, x2) == ["3fiflurg", "1seflurg", "2thflurg"]
+CODE
       end
     end
   end
