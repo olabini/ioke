b8eb9f842d6c1b334471844663842262ac7d9781
Ola Bini <ola.bini@gmail.com>
2008-12-10 12:52:22 +0100

    Start converting method_spec

--- test/method_spec.rb
+++ test/method_spec.rb
@@ -6,31 +6,6 @@ import Java::java.io.StringWriter unless defined?(StringWriter)
 import Java::java.io.InputStreamReader unless defined?(InputStreamReader)
 import Java::java.lang.System unless defined?(System)
 
-describe "DefaultBehavior" do 
-  describe "'method'" do 
-    it "should return a method that returns nil when called with no arguments" do 
-      ioke = IokeRuntime.get_runtime()
-      ioke.evaluate_stream(StringReader.new("method call")).should == ioke.nil
-      ioke.evaluate_stream(StringReader.new("method() call")).should == ioke.nil
-    end
-    
-    it "should name itself after the slot it's assigned to if it has no name" do 
-      ioke = IokeRuntime.get_runtime()
-      ioke.evaluate_stream(StringReader.new("x = method(nil)")).data.name.should == "x"
-    end
-    
-    it "should not change it's name if it already has a name" do 
-      ioke = IokeRuntime.get_runtime()
-      ioke.evaluate_stream(StringReader.new("x = method(nil)\ny = cell(\"x\")\ncell(\"y\")")).data.name.should == "x"
-    end
-    
-    it "should know it's own name" do 
-      ioke = IokeRuntime.get_runtime()
-      ioke.evaluate_stream(StringReader.new("(x = method(nil)) name")).data.text.should == "x"
-    end
-  end
-end
-
 describe "DefaultMethod" do 
   it "should be possible to give it a documentation string" do 
     ioke = IokeRuntime.get_runtime()
