5b5d5dcdad73aa861471c5174db9d93f3e17f291
Ola Bini <ola.bini@gmail.com>
2008-12-24 15:01:15 +0100

    Fix bug #111, so that build can happen on either git or hg

--- generate_filter_data.rb
+++ generate_filter_data.rb
@@ -1,6 +1,13 @@
 
 timestamp = Time.now.strftime("%Y-%m-%d %H:%M")
-commit = `git branch -v --no-color`.split[1..2].join(" ")
+
+commit = nil
+begin
+  commit_lines = `git branch -v --no-color`.to_a
+  commit = commit_lines.grep(/^\* /).first.split[2]
+rescue Exception
+  commit = `hg heads`.to_a[0].split[1]
+end
 
 File.open("version_data.properties", "w") do |f|
   f.puts("DATE=#{timestamp}")
