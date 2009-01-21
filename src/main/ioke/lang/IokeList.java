23c0130599de233e4fe001a77dedabc10bc4cd30
Sam Aaron <samaaron@gmail.com>
2009-01-21 23:33:22 +0100

    prune IokeList imports

--- src/main/ioke/lang/IokeList.java
+++ src/main/ioke/lang/IokeList.java
@@ -7,7 +7,6 @@ import java.util.Arrays;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Collections;
-import java.util.HashMap;
 import java.util.Map;
 
 import ioke.lang.exceptions.ControlFlow;
