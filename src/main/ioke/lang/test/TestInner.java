8e66ff1c4c8b130d2cd2e5293e058cf1e6ded566
Ola Bini <ola.bini@gmail.com>
2009-02-28 15:09:58 +0100

    Add tests for inner classes

---
+++ src/main/ioke/lang/test/TestInner.java
@@ -0,0 +1,16 @@
+/*
+ * See LICENSE file in distribution for copyright and licensing information.
+ */
+package ioke.lang.test;
+
+/**
+ *
+ * @author <a href="mailto:ola.bini@gmail.com">Ola Bini</a>
+ */
+public class TestInner {
+    public static class TheInner {
+        public String getFoo() {
+            return "an inner class";
+        }
+    }
+}// TestInner
