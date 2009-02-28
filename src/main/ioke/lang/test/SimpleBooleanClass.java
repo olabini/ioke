460186a546d5f1082c3ec5f68ad7e7174fa8e480
Ola Bini <ola.bini@gmail.com>
2009-02-28 15:26:31 +0100

    Make it possible to implement methods that return primitive booleans

---
+++ src/main/ioke/lang/test/SimpleBooleanClass.java
@@ -0,0 +1,14 @@
+/*
+ * See LICENSE file in distribution for copyright and licensing information.
+ */
+package ioke.lang.test;
+
+/**
+ *
+ * @author <a href="mailto:ola.bini@gmail.com">Ola Bini</a>
+ */
+public class SimpleBooleanClass {
+    public boolean doTheThing() {
+        return false;
+    }
+}// SimpleBooleanClass
