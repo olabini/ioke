05e37bab26ca79e3318d449873da4b8bae81f8f0
Ola Bini <ola.bini@gmail.com>
2009-09-08 18:43:08 +0200

    Sort out some more things to remove identity hash map

--- src/ikj/main/ioke/lang/IokeObject.java
+++ src/ikj/main/ioke/lang/IokeObject.java
@@ -26,7 +26,7 @@ public class IokeObject implements TypeChecker {
     private boolean frozen = false;
 
     // Used to handle recursive cell lookup chains more efficiently. Should in the end be a ThreadLocal of course.
-    private boolean marked = false;
+    public boolean marked = false;
 
     public IokeObject(Runtime runtime, String documentation) {
         this(runtime, documentation, IokeData.None);
