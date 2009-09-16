d2b0c54dd47a362ac3d5f1a803dcb51961d81a91
Ola Bini <ola.bini@gmail.com>
2009-09-16 14:17:35 -0700

    Add basic support for the mimicked hook

--- src/ikj/main/ioke/lang/Hook.java
+++ src/ikj/main/ioke/lang/Hook.java
@@ -108,6 +108,16 @@ public class Hook extends IokeData {
         }
     }
 
+    public static void fireMimicked(IokeObject on, IokeObject message, IokeObject context, IokeObject mimickingObject) throws ControlFlow {
+        Collection<IokeObject> hooks = on.hooks;
+        if(hooks != null) {
+            IokeObject mimickedMessage = context.runtime.mimickedMessage;
+            for(IokeObject h : hooks) {
+                ((Message)IokeObject.data(mimickedMessage)).sendTo(mimickedMessage, context, h);
+            }
+        }
+    }
+
     public static void init(final Runtime runtime) throws ControlFlow {
         final IokeObject obj = new IokeObject(runtime, "A hook allow you to observe what happens to a specific object. All hooks have Hook in their mimic chain.", new Hook(new ArrayList<IokeObject>()));
         obj.setKind("Hook");
