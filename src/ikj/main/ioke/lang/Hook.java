ffe26ccfcc612669c60f88e423a0ed8eeff3064f
Ola Bini <ola.bini@gmail.com>
2009-09-15 18:46:55 -0700

    Add beginnings of hook implementation

--- src/ikj/main/ioke/lang/Hook.java
+++ src/ikj/main/ioke/lang/Hook.java
@@ -3,17 +3,60 @@
  */
 package ioke.lang;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.HashMap;
+
 import ioke.lang.exceptions.ControlFlow;
 
 /**
  *
  * @author <a href="mailto:ola.bini@gmail.com">Ola Bini</a>
  */
-public class Hook  {
+public class Hook extends IokeData {
+    private List<IokeObject> connected;
+
+    public Hook(List<IokeObject> connected) {
+        this.connected = connected;
+    }
+
     public static void init(final Runtime runtime) throws ControlFlow {
-        IokeObject obj = new IokeObject(runtime, "A hook allow you to observe what happens to a specific object. All hooks have Hook in their mimic chain.");
+        final IokeObject obj = new IokeObject(runtime, "A hook allow you to observe what happens to a specific object. All hooks have Hook in their mimic chain.", new Hook(new ArrayList<IokeObject>()));
         obj.setKind("Hook");
         obj.mimicsWithoutCheck(runtime.origin);
         runtime.iokeGround.registerCell("Hook", obj);
+
+        obj.registerMethod(runtime.newNativeMethod("Takes one or more arguments to hook into and returns a new Hook connected to them.", new NativeMethod("into") {
+                private final DefaultArgumentsDefinition ARGUMENTS = DefaultArgumentsDefinition
+                    .builder()
+                    .withRequiredPositional("firstConnected")
+                    .withRest("restConnected")
+                    .getArguments();
+
+                @Override
+                public DefaultArgumentsDefinition getArguments() {
+                    return ARGUMENTS;
+                }
+
+                @Override
+                public Object activate(IokeObject method, IokeObject context, IokeObject message, Object on) throws ControlFlow {
+                    List<Object> args = new ArrayList<Object>();
+                    getArguments().getEvaluatedArguments(context, message, on, args, new HashMap<String, Object>());
+                    IokeObject hook = obj.allocateCopy(context, message);
+                    hook.mimicsWithoutCheck(obj);
+
+                    List<IokeObject> objs = new ArrayList<IokeObject>(args.size());
+                    for(Object o : args) {
+                        objs.add(IokeObject.as(o, context));
+                    }
+
+                    hook.setData(new Hook(objs));
+                    return hook;
+                }
+            }));
+    }
+
+    public IokeData cloneData(IokeObject obj, IokeObject m, IokeObject context) {
+        return new Hook(new ArrayList<IokeObject>(connected));
     }
 }// Hook
