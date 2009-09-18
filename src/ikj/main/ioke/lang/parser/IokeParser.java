677544a61929118de9a0ecdcf963c3d157ded61e
Ola Bini <ola.bini@gmail.com>
2009-09-18 15:25:30 -0700

    Add support for isIDDigit and isLetter parsing

--- src/ikj/main/ioke/lang/parser/IokeParser.java
+++ src/ikj/main/ioke/lang/parser/IokeParser.java
@@ -172,26 +172,39 @@ public class IokeParser {
     }
 
     private IokeObject parseEmptyMessageSend() {
-        // TODO: implement
-        return null;
+        List<Object> args = parseExpressionChain();
+        parseCharacter(')');
+        IokeObject mx = runtime.createMessage(new Message(runtime, ""));
+        Message.setArguments(mx, args);
+        return mx;
     }
 
     private IokeObject parseSquareMessageSend() {
-        // TODO: implement
-        return null;
+        List<Object> args = parseExpressionChain();
+        parseCharacter(']');
+        IokeObject mx = runtime.createMessage(new Message(runtime, "[]"));
+        Message.setArguments(mx, args);
+        return mx;
     }
 
     private IokeObject parseCurlyMessageSend() {
-        // TODO: implement
-        return null;
+        List<Object> args = parseExpressionChain();
+        parseCharacter('}');
+        IokeObject mx = runtime.createMessage(new Message(runtime, "{}"));
+        Message.setArguments(mx, args);
+        return mx;
     }
 
     private IokeObject parseSetMessageSend() {
-        // TODO: implement
-        return null;
+        parseCharacter('{');
+        List<Object> args = parseExpressionChain();
+        parseCharacter('}');
+        IokeObject mx = runtime.createMessage(new Message(runtime, "set"));
+        Message.setArguments(mx, args);
+        return mx;
     }
 
-    private IokeObject parseComment() {
+    private void parseComment() {
         // TODO: implement
         return null;
     }
@@ -232,13 +245,37 @@ public class IokeParser {
     }
 
     private boolean isLetter(int c) {
-        // TODO: implement
-        return false;
+        return ((c>='A' && c<='Z') ||
+                c=='_' ||
+                (c>='a' && c<='z') ||
+                (c>='\u00C0' && c<='\u00D6') ||
+                (c>='\u00D8' && c<='\u00F6') ||
+                (c>='\u00F8' && c<='\u1FFF') ||
+                (c>='\u2200' && c<='\u22FF') ||
+                (c>='\u27C0' && c<='\u27EF') ||
+                (c>='\u2980' && c<='\u2AFF') ||
+                (c>='\u3040' && c<='\u318F') ||
+                (c>='\u3300' && c<='\u337F') ||
+                (c>='\u3400' && c<='\u3D2D') ||
+                (c>='\u4E00' && c<='\u9FFF') ||
+                (c>='\uF900' && c<='\uFAFF'));
     }
 
-
     private boolean isIDDigit(int c) {
-        // TODO: implement
-        return false;
+        return ((c>='0' && c<='9') ||
+                (c>='\u0660' && c<='\u0669') ||
+                (c>='\u06F0' && c<='\u06F9') ||
+                (c>='\u0966' && c<='\u096F') ||
+                (c>='\u09E6' && c<='\u09EF') ||
+                (c>='\u0A66' && c<='\u0A6F') ||
+                (c>='\u0AE6' && c<='\u0AEF') ||
+                (c>='\u0B66' && c<='\u0B6F') ||
+                (c>='\u0BE7' && c<='\u0BEF') ||
+                (c>='\u0C66' && c<='\u0C6F') ||
+                (c>='\u0CE6' && c<='\u0CEF') ||
+                (c>='\u0D66' && c<='\u0D6F') ||
+                (c>='\u0E50' && c<='\u0E59') ||
+                (c>='\u0ED0' && c<='\u0ED9') ||
+                (c>='\u1040' && c<='\u1049'));
     }
 }
