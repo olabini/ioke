c99293b93be10c0b8693969dd4ea965232bc3e28
Ola Bini <ola.bini@gmail.com>
2009-10-02 11:40:13 +0100

    Sort out the final parts of fixing the value equality semantics. YAY.

--- src/ikj/main/ioke/lang/DefaultArgumentsDefinition.java
+++ src/ikj/main/ioke/lang/DefaultArgumentsDefinition.java
@@ -200,8 +200,6 @@ public class DefaultArgumentsDefinition {
         if(argCount < min || (max != -1 && argCount > (max+keySize))) {
             final int finalArgCount = argCount;
             if(argCount < min) {
-                System.err.println("OH NO... : ");
-                new Exception().printStackTrace();
                 final IokeObject condition = IokeObject.as(IokeObject.getCellChain(runtime.condition, 
                                                                                    message, 
                                                                                    context, 
@@ -290,8 +288,6 @@ public class DefaultArgumentsDefinition {
         while(argCount < min || (max != -1 && argCount > max)) {
             final int finalArgCount = argCount;
             if(argCount < min) {
-                System.err.println("OH NO... : ");
-                new Exception().printStackTrace();
                 final IokeObject condition = IokeObject.as(IokeObject.getCellChain(runtime.condition, 
                                                                              message, 
                                                                              context, 
@@ -463,6 +459,17 @@ public class DefaultArgumentsDefinition {
         return new DefaultArgumentsDefinition(new ArrayList<Argument>(), new ArrayList<String>(), null, null, 0, 0, false);
     }
 
+    public static int indexOf(List<Object> objs, Object obj) {
+        int i = 0;
+        for(Object o : objs) {
+            if(o == obj) {
+                return i;
+            }
+            i++;
+        }
+        return -1;
+    }
+
     public static DefaultArgumentsDefinition createFrom(List<Object> args, int start, int len, final IokeObject message, final Object on, final IokeObject context) throws ControlFlow {
         final Runtime runtime = context.runtime;
         List<Argument> arguments = new ArrayList<Argument>();
@@ -507,7 +514,7 @@ public class DefaultArgumentsDefinition {
                 arguments.add(new OptionalArgument(name, m.next));
             } else {
                 if(hadOptional) {
-                    int index = args.indexOf(obj) + start;
+                    int index = indexOf(args, obj) + start;
 
 
                     final IokeObject condition = IokeObject.as(IokeObject.getCellChain(runtime.condition, 
