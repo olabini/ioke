bff6423e2c90c533845aaf00b34ed586eadf67b3
Ola Bini <ola.bini@gmail.com>
2009-10-02 16:23:13 +0100

    Fix a stupid stupid stupid bug in equality

--- src/ikj/main/ioke/lang/IokeData.java
+++ src/ikj/main/ioke/lang/IokeData.java
@@ -120,7 +120,7 @@ public abstract class IokeData {
             boolean result = (other instanceof IokeObject) && (self.getCells() == IokeObject.as(other, self).getCells());
             return result;
         } else {
-            boolean result = IokeObject.isTrue(((Message)IokeObject.data(self.runtime.eqMessage)).sendTo(self.runtime.eqMessage, self.runtime.ground, self, other));
+            boolean result = IokeObject.isTrue(((Message)IokeObject.data(self.runtime.eqMessage)).sendTo(self.runtime.eqMessage, self.runtime.ground, self, self.runtime.createMessage(Message.wrap(IokeObject.as(other, self)))));
             return result;
         }
     }
