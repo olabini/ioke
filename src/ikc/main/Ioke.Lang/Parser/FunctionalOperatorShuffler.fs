dc50a9cfb08fd2b474c0ff3db2d0b11b23fad4f4
Ola Bini <ola.bini@gmail.com>
2009-10-03 10:28:38 +0100

    Remove type tags on IndexOf method

--- src/ikc/main/Ioke.Lang/Parser/FunctionalOperatorShuffler.fs
+++ src/ikc/main/Ioke.Lang/Parser/FunctionalOperatorShuffler.fs
@@ -53,7 +53,7 @@ type FunctionalOperatorShuffler(msg:IokeObject, context:IokeObject, message:Ioke
     let add_all (l1 : IList) l2 =
         Seq.iter (fun arg -> l1.Add(arg) |> ignore) (Seq.cast l2 : seq<Object>)
 
-//    let IndexOf (objs:IList<IokeObject>) (obj:IokeObject) =
+//    let IndexOf objs obj =
 //        match Seq.tryFindIndex (fun arg -> arg = obj) objs with
 //            | Some(num) -> num
 //            | None      -> -1
