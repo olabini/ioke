e5b1eb68ef00ea724fef3e020782efe776b68876
Ola Bini <ola.bini@gmail.com>
2009-10-12 18:01:25 +0200

    Sort out so that Range#each will return a sequence when called with no arguments

--- src/ikj/main/ioke/lang/Range.java
+++ src/ikj/main/ioke/lang/Range.java
@@ -255,7 +255,7 @@ public class Range extends IokeData {
         obj.registerMethod(runtime.newNativeMethod("takes either one or two or three arguments. if one argument is given, it should be a message chain that will be sent to each object in the range. the result will be thrown away. if two arguments are given, the first is an unevaluated name that will be set to each of the values in the range in succession, and then the second argument will be evaluated in a scope with that argument in it. if three arguments is given, the first one is an unevaluated name that will be set to the index of each element, and the other two arguments are the name of the argument for the value, and the actual code. the code will evaluate in a lexical context, and if the argument name is available outside the context, it will be shadowed. the method will return the range.", new NativeMethod("each") {
                 private final DefaultArgumentsDefinition ARGUMENTS = DefaultArgumentsDefinition
                     .builder()
-                    .withRequiredPositionalUnevaluated("indexOrArgOrCode")
+                    .withOptionalPositionalUnevaluated("indexOrArgOrCode")
                     .withOptionalPositionalUnevaluated("argOrCode")
                     .withOptionalPositionalUnevaluated("code")
                     .getArguments();
