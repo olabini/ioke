6dd773b296da3092a131e97e98cbd53f1c9f2c2c
Ola Bini <ola.bini@gmail.com>
2009-10-21 16:47:29 +0800

    Add support for generating tuple accessors, and thus make all tests run

--- src/builtin/D37_tuple.ik
+++ src/builtin/D37_tuple.ik
@@ -1,11 +1,47 @@
 
+Tuple mimic!(Mixins Comparing)
+
 Tuple Two   = Tuple mimic
+Tuple Two private:initializeWith(nil, nil)
+Tuple Two first  = Tuple private:accessor(0)
+Tuple Two _1     = Tuple private:accessor(0)
+Tuple Two second = Tuple private:accessor(1)
+Tuple Two _2     = Tuple private:accessor(1)
+
 Tuple Three = Tuple Two mimic
+Tuple Three private:initializeWith(nil, nil, nil)
+Tuple Three third  = Tuple private:accessor(2)
+Tuple Three _3     = Tuple private:accessor(2)
+
 Tuple Four  = Tuple Three mimic
+Tuple Four private:initializeWith(nil, nil, nil, nil)
+Tuple Four fourth  = Tuple private:accessor(3)
+Tuple Four _4      = Tuple private:accessor(3)
+
 Tuple Five  = Tuple Four mimic
+Tuple Five private:initializeWith(nil, nil, nil, nil, nil)
+Tuple Five fifth   = Tuple private:accessor(4)
+Tuple Five _5      = Tuple private:accessor(4)
+
 Tuple Six   = Tuple Five mimic
+Tuple Six private:initializeWith(nil, nil, nil, nil, nil, nil)
+Tuple Six sixth   = Tuple private:accessor(5)
+Tuple Six _6      = Tuple private:accessor(5)
+
 Tuple Seven = Tuple Six mimic
+Tuple Seven private:initializeWith(nil, nil, nil, nil, nil, nil, nil)
+Tuple Seven seventh  = Tuple private:accessor(6)
+Tuple Seven _7       = Tuple private:accessor(6)
+
 Tuple Eight = Tuple Seven mimic
+Tuple Eight private:initializeWith(nil, nil, nil, nil, nil, nil, nil, nil)
+Tuple Eight eighth  = Tuple private:accessor(7)
+Tuple Eight _8      = Tuple private:accessor(7)
+
 Tuple Nine  = Tuple Eight mimic
+Tuple Nine private:initializeWith(nil, nil, nil, nil, nil, nil, nil, nil, nil)
+Tuple Nine ninth   = Tuple private:accessor(8)
+Tuple Nine _9      = Tuple private:accessor(8)
+
 Tuple Many  = Tuple Nine mimic
 
--- src/ikj/main/ioke/lang/Tuple.java
+++ src/ikj/main/ioke/lang/Tuple.java
@@ -3,6 +3,7 @@
  */
 package ioke.lang;
 
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
 import java.util.HashMap;
@@ -26,5 +27,54 @@ public class Tuple extends IokeData {
         obj.setKind("Tuple");
         obj.mimicsWithoutCheck(runtime.origin);
         runtime.iokeGround.registerCell("Tuple", obj);
+
+        obj.registerMethod(runtime.newNativeMethod("will modify the tuple, initializing it to contain the specified arguments", new NativeMethod("private:initializeWith") {
+                private final DefaultArgumentsDefinition ARGUMENTS = DefaultArgumentsDefinition
+                    .builder()
+                    .withRest("values")
+                    .getArguments();
+
+                @Override
+                public DefaultArgumentsDefinition getArguments() {
+                    return ARGUMENTS;
+                }
+
+                @Override
+                public Object activate(IokeObject method, IokeObject context, IokeObject message, Object on) throws ControlFlow {
+                    List<Object> args = new ArrayList<Object>();
+                    getArguments().getEvaluatedArguments(context, message, on, args, new HashMap<String, Object>());
+
+                    Object[] o = new Object[args.size()];
+                    IokeObject.as(on, context).setData(new Tuple(args.toArray(o)));
+
+                    return on;
+                }
+            }));
+
+        obj.registerMethod(runtime.newNativeMethod("returns a new method that can be used to access an element of a tuple based on the index", new NativeMethod("private:accessor") {
+                private final DefaultArgumentsDefinition ARGUMENTS = DefaultArgumentsDefinition
+                    .builder()
+                    .withRequiredPositional("index")
+                    .getArguments();
+
+                @Override
+                public DefaultArgumentsDefinition getArguments() {
+                    return ARGUMENTS;
+                }
+
+                @Override
+                public Object activate(IokeObject method, IokeObject context, IokeObject message, Object on) throws ControlFlow {
+                    List<Object> args = new ArrayList<Object>();
+                    getArguments().getEvaluatedArguments(context, message, on, args, new HashMap<String, Object>());
+                    final int index = Number.extractInt(args.get(0), message, context);
+                    return runtime.newNativeMethod("Returns the object at index " + index + " in the receiving tuple", new NativeMethod.WithNoArguments("_" + index) {
+                            @Override
+                            public Object activate(IokeObject method, IokeObject context, IokeObject message, Object on) throws ControlFlow {
+                                getArguments().getEvaluatedArguments(context, message, on, new ArrayList<Object>(), new HashMap<String, Object>());
+                                return ((Tuple)IokeObject.data(on)).elements[index];
+                            }
+                        });
+                }
+            }));
     }
 }
